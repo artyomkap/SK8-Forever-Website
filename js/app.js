198985b26bddfcce8d2836746dd0329330cbe6c5
artyomkap <103292137+artyomkap@users.noreply.github.com>
2022-04-12 12:09:08 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,57 @@
+const sliderMain = new Swiper('.slider_main', {
+	freeMode: true,
+	centeredSlides: true,
+	mousewheel: true,
+	parallax: true,
+	breakpoints: {
+		0: {
+			slidesPerView: 2.5,
+			spaceBetween: 20
+		},
+		680: {
+			slidesPerView: 3.5,
+			spaceBetween: 60
+		}
+	}
+})
+const sliderBg = new Swiper('.slider_bg', {
+	freeMode: true,
+	centeredSlides: true,
+	mousewheel: true,
+	parallax: true,
+	breakpoints: {
+		0: {
+			slidesPerView: 2.5,
+			spaceBetween: 20
+		},
+		680: {
+			slidesPerView: 3.5,
+			spaceBetween: 60
+		}
+	}
+	
+})
+
+sliderMain.controller.control = sliderBg
+
+document.querySelectorAll('.slider__item'). forEach(item => {
+	item.addEventListener('click', event => {
+		item.classList.toggle('opened')
+	})
+})
+
+let desc = document.querySelector('.description')
+sliderMain.on('slideChange', e => {
+	sliderMain.activeIndex > 0 ? desc.classList.add('hidden') : desc.classList.remove('hidden')
+})
+
+var sliderVideoMain = new Swiper('.swiper-container', {		
+	centeredSlides: true,
+	slidesPerView: 1,	
+	grabCursor: true,
+	loop: true,
+	navigation: {
+		nextEl: ".showcase-navigarion__next",
+		prevEl: ".showcase-navigarion__prev",
+	}
+})
